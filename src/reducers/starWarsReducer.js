38662d5ef5cb984cb26c2bf6eea3e008fa17eaf7
Kevin Grant <ConsciousK@yahoo.com>
2019-07-18 20:36:04 -0400

    MVP met.

--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -13,13 +13,13 @@ export const charsReducer = (state = initialState, action) => {
   switch (action.type) {
     case FETCHING:
       return { ...state, fetching: true};
-    case FAILURE:
-      return { ...state, fetching: false, error: action.payload};
     case SUCCESS:
       return { 
         ...state,
         characters: [...state.characters, ...action.payload],
         fetching: false };
+    case FAILURE:
+      return { ...state, fetching: false, error: action.payload};
     // Fill me in with the important reducers
     // action types should be FETCHING, SUCCESS and FAILURE
     // your switch statement should handle all of these cases.
--- src/views/CharacterListView.js
+++ src/views/CharacterListView.js
@@ -3,20 +3,20 @@ import { connect } from "react-redux";
 
 import { CharacterList } from "../components";
 // import actions
+import { getCharacters } from "../actions";
 
 class CharacterListView extends React.Component {
-  constructor() {
-    super();
-  }
 
   componentDidMount() {
     // call our action
+    this.props.getCharacters();
   }
 
   render() {
     if (this.props.fetching) {
-      // return something here to indicate that you are fetching data
-    }
+      return <h2>Information Loading...</h2>;
+
+    };
     return (
       <div className="CharactersList_wrapper">
         <CharacterList characters={this.props.characters} />
@@ -27,9 +27,18 @@ class CharacterListView extends React.Component {
 
 // our mapStateToProps needs to have two properties inherited from state
 // the characters and the fetching boolean
+const mapStateToProps = state => {
+  console.log(state);
+  return {
+    characters: state.charsReducer.characters,
+    error: state.charsReducer.error,
+    fetching: state.charsReducer.fetching
+  };
+};
+
 export default connect(
-  null /* mapStateToProps replaces null here */,
+  mapStateToProps, /* mapStateToProps replaces null here */
   {
-    /* action creators go here */
+    getCharacters/* action creators go here */
   }
 )(CharacterListView);
